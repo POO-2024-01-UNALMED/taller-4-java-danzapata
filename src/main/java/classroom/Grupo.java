9ce037265b13e449403f2ab0a0496317ca9255f1
danzapata <142749695+danzapata@users.noreply.github.com>
2024-03-05 11:11:44 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -1,15 +1,14 @@
 package classroom;
-import java.util.ArrayList;
 
 public class Grupo {
 
-    public ArrayList<Persona> estudiantes = new ArrayList<>();
+    public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
     public int codigo = 0;
     public String horario;
 
-    public Grupo(ArrayList<Persona> estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
+    public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
@@ -19,32 +18,28 @@ public class Grupo {
 
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        for (int i=0; i==cantidadEstudiantes; i++){
-            Persona estu = new Persona("Sin nombre");
-            this.estudiantes.add(estu);
-        } 
+        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario );
+     
         this.profesor = profesor;
         this.asignatura = asignatura;
         this.codigo = codigo;
         this.horario = horario;
     }
 
-    public Grupo(ArrayList<Persona> estudiantes, Persona profesor, Asignatura asignatura) {
+    public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
     }
 
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
-        for (int i = 0; i < estudiantes.size(); i++) {
-            if (estudiantes.get(i).getCedula() == estudianteViejo.getCedula()) {
-                estudiantes.set(i, estudianteNuevo);
-                break;
+        for (int i=0; i==estudiantes.length; i++){
+            if (estudiantes[i].equals(estudianteViejo)){
+                estudiantes[i] = estudianteNuevo;
             }
         }
     }
     
     void cambiarEstudiante(int indice, Persona estudiante) {
-        estudiantes.set(indice, estudiante);
-    }
-}
+        estudiantes[indice]= estudiante;
+}}
